3acf2ae7ef4ae99a314311ac5df2138a49f12d37
manyakhachatryan <manya.khachatryan1@gmail.com>
2022-08-22 00:40:47 +0400

    implement the same using Constructor function

---
+++ Practice/Repeat Classes Promise Dom/ex3.js
@@ -0,0 +1,16 @@
+// 1) implement the same using Constructor function
+
+
+class Man1{
+    constructor(name, surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
+
+function Man2 (name, surname)
+{
+
+this.name = name;
+this.surname = surname;
+}
